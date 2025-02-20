e23faec0e07d093f00c3e16fb6263dcbdcba5d60
AnirudhXrajput <131905729+AnirudhXrajput@users.noreply.github.com>
2025-02-20 18:09:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ app.use(express.urlencoded({extended:true}));
 app.get('/',(req,res)=>{
     res.json({message:"hello worls"});
 });
-
+app.get("/hello",(req,res)=>{
+    res.json({name:"anirudh"});
+})
 app.post('/users',(req,res)=>{
     const user = req.body;
     res.json({message:"User created",user});
